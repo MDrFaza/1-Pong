553648cc37d84b9a501769913e202ee318b00a66
MDrFaza <mdrfaza21@gmail.com>
2021-09-07 21:01:23 +0700

    first commit

---
+++ Project/Assets/Scripts/BallControl.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class BallControl : MonoBehaviour
+{
+    // Rigidbody 2D bola
+    private Rigidbody2D rigidBody2D;
+ 
+    // Besarnya gaya awal yang diberikan untuk mendorong bola
+    public float xInitialForce;
+    public float yInitialForce;
+
+    // Titik asal lintasan bola saat ini
+    private Vector2 trajectoryOrigin;
+
+    void ResetBall()
+    {
+        // Reset posisi menjadi (0,0)
+        transform.position = Vector2.zero;
+ 
+        // Reset kecepatan menjadi (0,0)
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+    void PushBall()
+    {
+        float randomDirectionX = Random.Range(-1f, 1f);
+        float randomDirectionY = Random.Range(-1f, 1f);
+        
+        float force = Mathf.Sqrt(Mathf.Pow(xInitialForce, 2) + Mathf.Pow(yInitialForce,2));
+        Debug.Log(force);
+        rigidBody2D.AddForce(force * new Vector2(randomDirectionX, randomDirectionY).normalized);
+    }
+
+    void RestartGame()
+    {
+        // Kembalikan bola ke posisi semula
+        ResetBall();
+ 
+        // Setelah 2 detik, berikan gaya ke bola
+        Invoke("PushBall", 2);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trajectoryOrigin = transform.position;
+        
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        trajectoryOrigin = transform.position;
+ 
+        // Mulai game
+        RestartGame();
+    }
+
+    // Ketika bola beranjak dari sebuah tumbukan, rekam titik tumbukan tersebut
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+
+    // Untuk mengakses informasi titik asal lintasan
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+
+}
